03f57dce7af861f84c6c269c57fa06a8a422fa69
Dmitry Iv. <df.creative@gmail.com>
2025-06-16 18:17:15 -0400

    Add french

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ const targets = document.querySelectorAll(".track-visible");
 targets.forEach(function (target) {
   observer.observe(target);
 });
+
+
+// carousel
+const scrollContainer = document.querySelector('#carousel');
+
+let scrollInterval;
+
+function autoScroll() {
+  scrollInterval = setInterval(() => {
+    carousel.scrollLeft += 1;
+    if (carousel.scrollLeft >= carousel.scrollWidth - carousel.clientWidth) {
+      carousel.scrollLeft = 0;
+    }
+  }, 16); // ~60fps
+}
+
+carousel.addEventListener('mouseenter', () => clearInterval(scrollInterval));
+carousel.addEventListener('mouseleave', autoScroll);
+
+autoScroll();
